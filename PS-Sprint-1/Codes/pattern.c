78bae8681fc340a5dbee2b98e4f3ebed61d07370
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-09-12 22:48:26 +0530

    Update pattern.c

--- PS-Sprint-1/Codes/pattern.c
+++ PS-Sprint-1/Codes/pattern.c
@@ -1,1 +1,19 @@
+#include<STDIO.H>
+
+int main(int argc, char const *argv[])
+{
+    int suku=1,r=4,bk=1;
+    for (int i = 1; i < r||bk<r; i++)
+    {
+        printf("%d ",suku++);
+        if (i%3==0)
+        {
+           printf("\n");
+           bk++;
+        }
+        
+    }
+    
+    return 0;
+}
 
