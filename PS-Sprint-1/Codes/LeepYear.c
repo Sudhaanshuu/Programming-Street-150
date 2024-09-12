cefa38d962d806fb2857501425dbdba71553a090
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-09-12 22:40:50 +0530

    Update LeepYear.c

--- PS-Sprint-1/Codes/LeepYear.c
+++ PS-Sprint-1/Codes/LeepYear.c
@@ -1,1 +1,21 @@
 
+#include <STDIO.H>
+int checkLeap(int y)
+{
+    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+        return 1;
+    else
+        return 0;
+}
+int main(int argc, char const *argv[])
+{
+    printf("Enter the Year ");
+    int y;
+    scanf("%d", &y);
+    if (checkLeap(y))
+        printf("Its leep");
+    else
+        printf("Not leep");
+
+    return 0;
+}
